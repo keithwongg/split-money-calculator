813227fcb9871f4fe403e7d4f1ee3f0f5a2f4de3
Keith Wong <keithwong@Keiths-MacBook-Air.local>
2024-12-22 11:22:17 +0800

    Refactor

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const NAMES_KEY = "names";
-var DATA = new Map();
+const ITEMS_KEY = "items";
+/*
+schema:
+
+names: ["person 1", "person 2"]
+
+items: [{
+    description: "item1",
+    cost: "20",
+    who_paid: "person 1",
+    to_receive_from: ["person 1", "person 2"]
+}]
+
+summary: {
+    "person 1" : balance
+    "person 2" : balance
+}
+*/
 
 window.onload = function (e) {
-    renderDataInUi()
+    renderNamesInUi()
 }
 
 function addName() {
@@ -10,34 +27,72 @@ function addName() {
     if (name.value.length <= 0) {
         return;
     }
-    DATA.set(name.value, {})
-    name.value = ''
-
-    let names = renderDataInUi()
-    console.log(`monkey: ${names}`)
-    resaveInLocalStorage(NAMES_KEY, names)
+    saveName(name.value)
+    renderNamesInUi()
 }
 
-function renderDataInUi() {
-    var names = getListOfNames()
+/*
+    NAMES
+*/
+function saveName(name) {
+    let names = getFromLocalStorage(NAMES_KEY)
+    if (names === undefined) {
+        saveInLocalStorage(NAMES_KEY, [name])
+        return
+    }
+    let namesArray = JSON.parse(names)
+    namesArray.push(name)
+    saveInLocalStorage(NAMES_KEY, namesArray)
+}
+
+function removeName(name) {
+    let names = getFromLocalStorage(NAMES_KEY)
+    if (names === undefined) {
+        return;
+    }
+    let namesArray = JSON.parse(names)
+    let index = namesArray.indexOf(name)
+    if (index > -1) {
+        namesArray.splice(index, 1)
+    }
+    saveInLocalStorage(NAMES_KEY, namesArray)
+    return namesArray
+}
+
+function renderNamesInUi() {
+    if (getFromLocalStorage(NAMES_KEY) === undefined) {
+        return
+    }
+    let names = JSON.parse(getFromLocalStorage(NAMES_KEY))
+    console.log(`monkey: ${names}`)
     renderListOfNames(names)
     renderDropdownOptions(names)
-    return names
-}
-
-function getListOfNames() {
-    let names = Array.from(DATA.keys())
-    console.log(names)
-    if (names.length <= 0) {
-        names = JSON.parse(localStorage.getItem(NAMES_KEY))
-    }
-    return names
 }
 
 function renderListOfNames(names) {
     var listOfNames = document.getElementById('names-list-text')
-    // this part need to add pill button for each name with the x to drop the name
-    listOfNames.innerHTML = `List of Names: ${names}`
+    listOfNames.innerHTML = ''
+    for(let i = 0; i < names.length; i++) {
+        listOfNames.appendChild(namePillButtonCreate(names[i]))
+    }
+}
+
+function namePillButtonCreate(content) {
+        let pill = document.createElement('button')
+        pill.value = content
+        pill.innerHTML = content
+        pill.classList.add('rounded-md', 'bg-blue-100', 'mb-2', 'px-2', 'flex')
+
+        let del = document.createElement('button')
+        del.innerHTML = 'X'
+        del.classList.add('rounded-md', 'bg-red-100', 'px-2', 'ml-2')
+        del.onclick = function () {
+            removeName(content)
+            renderNamesInUi()
+        }
+
+        pill.appendChild(del)
+        return pill
 }
 
 function renderDropdownOptions(names) {
@@ -52,8 +107,11 @@ function renderDropdownOptions(names) {
     }
 }
 
-function resaveInLocalStorage(key, content) {
-    localStorage.removeItem(key)
+function getFromLocalStorage(key) {
+    return localStorage.getItem(key) ?? undefined
+}
+
+function saveInLocalStorage(key, content) {
     localStorage.setItem(key, JSON.stringify(content))
 }
 
@@ -61,7 +119,15 @@ function addItemsAndCost() {
     var dropdownName = document.getElementById('opts-name')
     var item = document.getElementById('item')
     var cost = document.getElementById('cost')
+    addToData(dropdownName.value, item.value, cost.value)
     renderLog(dropdownName.value, item.value, cost.value)
+    calculateCosts()
+}
+
+function addToData(name, itemDesc, cost) {
+    let dataObj = Object.fromEntries(DATA)
+    dataObj[name].items.push({description: itemDesc, cost: cost})
+    saveInLocalStorage(DATA_KEY, dataObj)
 }
 
 function renderLog(name, item, cost) {
@@ -83,12 +149,13 @@ function renderLog(name, item, cost) {
     logTable.appendChild(tr)
 }
 
-
-function calculateCosts() {
-
-}
-
 function clearAll() {
     localStorage.removeItem(NAMES_KEY)
     location.reload()
+}
+
+function calculateCosts() {
+    for(let item in DATA) {
+        console.log(item.key, JSON.stringify(item.value))
+    }
 }
